6da827ef0cf662ba48f498db75f2c29d0eccc9f3
Kaladhar Royal <kaladharroyal@gmail.com>
2026-01-28 11:44:14 +0530

    updated day28

--- day28/ContextApi/src/contexts/ThemeProvider.jsx
+++ day28/ContextApi/src/contexts/ThemeProvider.jsx
@@ -1,14 +1,15 @@
-import React, {useState} from "react";
-import { ThemeContext } from "./ThemeContext";
+import React, { useState} from 'react';
+import { ThemeContext } from './ThemeContext';
+
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('dark');
 
 
-const ThemeProvider = ({childern}) =>{
-    const [theme, setTheme]  = useState('dark')
-    return(
-        <ThemeContext.Provider value = {{theme, setTheme}}>
-            {children}
-        </ThemeContext.Provider>
-
-    );
+  return (
+    <ThemeContext.Provider value={{theme,setTheme}}>
+      {children}
+    </ThemeContext.Provider>
+  );
 };
+
 export default ThemeProvider;
