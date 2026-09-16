5a951fbf896dd27190205fad99bc62392359f133
Kaladharroyal <kaladharroyal@gmail.com>
2026-09-16 10:16:01 +0530

    refactor: clean up legacy day folder references

--- day28/protected-routing/src/components/ProtectedRoute.jsx
+++
@@ -1,7 +0,0 @@
-import {Navigate} from  "react-router-dom"
-function ProtectedRoute({children}){
-    const isAuthenticated = true;
-    return isAuthenticated ? children : <Navigate to = "/login" replace />
-}
-
-export default ProtectedRoute
